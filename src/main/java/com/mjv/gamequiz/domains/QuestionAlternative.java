9ae5bbe8fc8f74a68b72413d0a7d2d337b59e079
Thallys Cezar <thalys_cezar@hotmail.com>
2024-02-05 17:02:38 -0300

    refactor: reconfig database, add PostgreSQL

--- src/main/java/com/mjv/gamequiz/domains/QuestionAlternative.java
+++ src/main/java/com/mjv/gamequiz/domains/QuestionAlternative.java
@@ -15,7 +15,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "t_question-alternative")
+@Table(name = "t_question_alternative")
 public class QuestionAlternative implements Serializable {
     private static final long serialVersionUID = 1L;
 
